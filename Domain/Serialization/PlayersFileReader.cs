950a6993276ce5b127219cc8569946a0b636c3c8
dhayd <dhayd.code@gmail.com>
2018-02-26 13:02:56 -0700

    players.json added as a resource

--- Domain/Serialization/PlayersFileReader.cs
+++ Domain/Serialization/PlayersFileReader.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace Domain.Serialization
@@ -8,9 +10,10 @@ namespace Domain.Serialization
     {
         public IEnumerable<PlayerData> Get()
         {
-            var fileName = @"..\players.json";
+            var assembly = Assembly.GetAssembly(typeof(PlayersRepository));
+            var resourceStream = assembly.GetManifestResourceStream("Domain.Serialization.players.json");
             string content;
-            using (var sr = new StreamReader(fileName))
+            using (var sr = new StreamReader(resourceStream, Encoding.UTF8))
             {
                 content = sr.ReadToEnd();
             }
